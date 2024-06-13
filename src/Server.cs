7d6fa901813ce866044e6a439ceb19adeec88f14
Eirik Grandahl <eirikjg@hotmail.com>
2024-06-13 17:07:16 +0200

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -9,5 +9,16 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = server.AcceptSocket();
-var response = "HTTP/1.1 200 OK\r\n\r\n";
-socket.Send(Encoding.UTF8.GetBytes(response));
+
+var buffer = new byte[1024];
+socket.Receive(buffer);
+
+var lines = ASCIIEncoding.ASCII.GetString(buffer).Split("\r\n");
+
+if (lines.Any() && lines[0].Split(" ")[1] != "/")
+{
+    socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
+} else
+{
+    socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+}
